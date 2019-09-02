4ad03cc78f50931be6e233a4561a84c227aa1908
cdanh-aptech <cdanha18037@cusc.ctu.edu.vn>
2019-09-02 19:30:28 +0700

    Index template

--- resources/views/layout/service.blade.php
+++ resources/views/layout/service.blade.php
@@ -1,90 +1,74 @@
 <div id="qbootstrap-services">
-        <div class="container">
-            <div class="row animate-box">
-                <div class="col-md-12 text-center qbootstrap-heading">
-                    <h2><b>CUỘC THI ẢNH LOGISTICS QUỐC TẾ LẦN THỨ I</b></h2>
+    <div class="container">
+        <div class="row animate-box">
+            <div class="col-md-12 col-md-offset-0 text-center qbootstrap-heading">
+            <h2><b>THÔNG TIN CUỘC THI ẢNH LOGISTICS QUỐC TẾ LẦN THỨ I</b></h2>
+            </div>
+        </div>
+        <div class="row">
+            {{-- Cơ cấu giải thưởng --}}
+            <div class="col-md-6 animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-params"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">CƠ CẤU GIẢI THƯỞNG</a></h3>
+                        <p>
+                                01 Giải Nhất: 1,000 USD/ giải       <br>
+                                02 Giải Nhì: 700 USD/ giải          <br>
+                                02 Giải Ba: 500 USD/ giải           <br>
+                                02 Giải Ấn tượng: 500 USD/ giải     <br>
+                                05 Giải Khuyến khích: 300 USD/ giải
+                        </p>
+                    </div>
                 </div>
             </div>
-
-            {{-- <div>
-                <a href="" class="News">
-                    <img class="St_img_Thumb_viewer" src="/data/news/thumb/resize_21232f297a57a5a743894a0e4a801fc3_201971672547.jpg">
-                </a>
-                <p class="St_H_Title">
-                    <a href="#">
-                        THỂ LỆ CUỘC THI ẢNH ẢNH LOGISTICS QUỐC TẾ LẦN THỨ I TẠI VIỆT NAM
-                    </a>
-                </p>
-                <p class="St_Intro_TT">
-                    Ảnh dự thi là những bức ảnh có giá trị về mặt nội dung và nghệ thuật phản ánh về các hoạt động trong lĩnh vực logistics tại việt Nam hoặc các nước trong khu vực Đông Nam Á.
-                </p>
-            </div> --}}
-
-            <div class="row">
-                {{-- Cơ cấu giải thưởng --}}
-                <div class="col-md-6 animate-box">
-                    <div class="services">
-                        <span class="icon">
-                            <i class="icon-book"></i>
-                        </span>
-                        <div class="desc">
-                            <h3><a href="#">CƠ CẤU GIẢI THƯỞNG</a></h3>
-                            <p>
-                                    01 Giải Nhất: 1,000 USD/ giải       <br>
-                                    02 Giải Nhì: 700 USD/ giải          <br>
-                                    02 Giải Ba: 500 USD/ giải           <br>
-                                    02 Giải Ấn tượng: 500 USD/ giải     <br>
-                                    05 Giải Khuyến khích: 300 USD/ giải
-                            </p>
-                        </div>
+            {{-- Hỗ trợ gửi ảnh --}}
+            <div class="col-md-6 animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-upload"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">HỖ TRỢ GỬI ẢNH</a></h3>
+                        <p>
+                                - Hỗ trợ gửi ảnh 24/7 - <br>
+                                Duy Anh +84 932 80 29 29
+                        </p>
                     </div>
                 </div>
-                {{-- Hỗ trợ gửi ảnh --}}
-                <div class="col-md-6 animate-box">
-                    <div class="services">
-                        <span class="icon">
-                            <i class="icon-upload"></i>
-                        </span>
-                        <div class="desc">
-                            <h3><a href="#">HỖ TRỢ GỬI ẢNH</a></h3>
-                            <p>
-                                Hỗ trợ gửi ảnh 24/7 - Duy Anh +84 932 80 29 29
-                            </p>
-                        </div>
+            </div>
+        </div>
+        <div class="row">
+            {{-- Hội đồng giám khảo --}}
+            <div class="col-md-6 animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-group"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">HỘI ĐỒNG GIÁM KHẢO</a></h3>
+                        <p>
+                            
+                        </p>
                     </div>
                 </div>
+            </div>
+            {{-- Ban tổ chức --}}
+            <div class="col-md-6 animate-box">
+                <div class="services">
+                    <span class="icon">
+                        <i class="icon-flow-merge"></i>
+                    </span>
+                    <div class="desc">
+                        <h3><a href="#">BAN TỔ CHỨC</a></h3>
+                        <p>
 
-                {{-- Hội đồng giám khảo --}}
-                <div class="col-md-6 animate-box">
-                    <div class="services">
-                        <span class="icon">
-                                <i class="icon-group"></i>
-                        </span>
-                        <div class="desc">
-                            <h3><a href="#">HỘI ĐỒNG GIÁM KHẢO</a></h3>
-                            {{--  --}}
-                            <p>
-                                    
-                            </p>
-                        </div>
+                        </p>
                     </div>
                 </div>
-                {{-- Ban Tổ Chức --}}
-                <div class="col-md-6 animate-box">
-                    <div class="services">
-                        <span class="icon">
-                            <i class="icon-flow-merge"></i>
-                        </span>
-                        <div class="desc">
-                            <h3><a href="#">BAN TỔ CHỨC</a></h3>
-                            {{--  --}}
-                            <p>
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
             </div>
         </div>
     </div>
+</div>
